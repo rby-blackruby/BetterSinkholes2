b18ac539076ee378e973b30e7261b96146297fc9
blackruby <58123929+rby-blackruby@users.noreply.github.com>
2020-02-02 12:29:08 +0100

    Removed unnecessary config options.

--- Plugin.cs
+++ Plugin.cs
@@ -12,12 +12,11 @@ namespace BetterSinkholes
         public static float SlowDistance;
         public static string TeleportMessage;
         public static uint TeleportMessageDuration;
-        public static bool EnableDebug;
 
         public static HarmonyInstance HarmonyInstance { set; get; }
         public override void OnEnable()
         {
-            Plugin.Info("Whatever.");
+            
             EnableBetterSinkholes = Plugin.Config.GetBool("bs_enable", true);
 
             if(EnableBetterSinkholes == false)
@@ -29,8 +28,7 @@ namespace BetterSinkholes
             SlowDistance = Plugin.Config.GetFloat("bs_slow_distance", 1.15f);
             TeleportDistance = Plugin.Config.GetFloat("bs_teleport_distance", 0.7f);
             TeleportMessage = Plugin.Config.GetString("bs_teleport_message", null);
-            TeleportMessageDuration = Plugin.Config.GetUInt("bs_teleport_message_duration", 5U);
-            EnableDebug = Plugin.Config.GetBool("bs_enable_debug", true);
+            TeleportMessageDuration = Plugin.Config.GetUInt("bs_teleport_message_duration", 5U);;
 
             HarmonyInstance = HarmonyInstance.Create($"blackruby.exiled.bettersinkholes");
             HarmonyInstance.PatchAll();
