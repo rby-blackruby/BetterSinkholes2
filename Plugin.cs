ef65e504a646dc870463893c86b66facd5cdbecb
blackruby <58123929+rby-blackruby@users.noreply.github.com>
2020-02-02 23:16:02 +0100

    Fixed reload issue.

--- Plugin.cs
+++ Plugin.cs
@@ -5,13 +5,14 @@ namespace BetterSinkholes
 {
     public class Plugin : EXILED.Plugin
     {
-        public override string getName { get; } = "Better Sinkholes";
+        public override string getName { get; } = "Better Sinkholes - blackruby";
 
         public static bool EnableBetterSinkholes;
         public static float TeleportDistance;
         public static float SlowDistance;
         public static string TeleportMessage;
         public static uint TeleportMessageDuration;
+        public static uint PatchCount;
 
         public static HarmonyInstance HarmonyInstance { set; get; }
         public override void OnEnable()
@@ -25,12 +26,14 @@ namespace BetterSinkholes
                 return;
             }
 
+            Plugin.Info("Thank you for using BetterSinkholes - blackruby");
             SlowDistance = Plugin.Config.GetFloat("bs_slow_distance", 1.15f);
             TeleportDistance = Plugin.Config.GetFloat("bs_teleport_distance", 0.7f);
-            TeleportMessage = Plugin.Config.GetString("bs_teleport_message", null);
+            TeleportMessage = Plugin.Config.GetString("bs_teleport_message", "You fell into the pocket dimension.");
             TeleportMessageDuration = Plugin.Config.GetUInt("bs_teleport_message_duration", 5U);;
 
-            HarmonyInstance = HarmonyInstance.Create($"blackruby.exiled.bettersinkholes");
+            HarmonyInstance = HarmonyInstance.Create($"blackruby.exiled.bettersinkholes{PatchCount}");
+            PatchCount++;
             HarmonyInstance.PatchAll();
 
         }
