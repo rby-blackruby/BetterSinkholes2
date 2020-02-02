b22d791427291b6998ff0abcb2c72e0814ac0ba0
blackruby <58123929+rby-blackruby@users.noreply.github.com>
2020-02-02 11:02:58 +0100

    BetterSinkholes-v1 RELEASE

---
+++ ImproveThoseSinkholesBaby.cs
@@ -0,0 +1,64 @@
+﻿using Mirror;
+using UnityEngine;
+using Harmony;
+using RemoteAdmin;
+using System;
+
+namespace BetterSinkholes
+{
+    [HarmonyPatch(typeof(SinkholeEnvironmentalHazard), "DistanceChanged", new Type[] { typeof(GameObject) })]
+    public class ImproveThoseSinkholesBaby
+    {
+        public Plugin plugin;
+        public ImproveThoseSinkholesBaby(Plugin plugin) => this.plugin = plugin;
+        public static bool Prefix(SinkholeEnvironmentalHazard __instance, GameObject player)
+        {
+            
+            if (!NetworkServer.active)
+            {
+                Plugin.addDebug("NetworkServer is not active.");
+                return false;
+            }
+
+            PlayerEffectsController componentInParent = player.GetComponentInParent<PlayerEffectsController>();
+            if(componentInParent == null)
+            {
+                Plugin.addDebug("ComponentInParent is null.");
+                return false;
+            }
+
+            if ((double)Vector3.Distance(player.transform.position, __instance.transform.position) > (double)__instance.DistanceToBeAffected * Plugin.SlowDistance)
+            {
+                componentInParent.DisableEffect("SinkHole");
+                Plugin.addDebug("SinkHole effect has been disabled for componentInParebt");
+                return false;
+            }
+
+            if(__instance.SCPImmune)
+            {
+                CharacterClassManager component = player.GetComponent<CharacterClassManager>();
+                if(component == null || component.IsAnyScp())
+                {
+                    Plugin.addDebug("SCP immunity detected.");
+                    return false;
+                }
+            }
+
+            if (Plugin.EnableBetterSinkholes && (double)Vector3.Distance(player.transform.position, __instance.transform.position) < (double)__instance.DistanceToBeAffected * Plugin.TeleportDistance)
+            {
+                player.GetComponent<PlyMovementSync>().OverridePosition(Vector3.down * 1998.5f, 0f, true);
+                PlayerEffectsController componentInParent2 = player.GetComponentInParent<PlayerEffectsController>();
+                componentInParent2.DisableEffect("SinkHole");
+                componentInParent2.GetEffect<CustomPlayerEffects.Corroding>("Corroding").isInPd = true;
+                componentInParent2.EnableEffect("Corroding");
+                QueryProcessor.Localplayer.GetComponent<Broadcast>().TargetAddElement(player.gameObject.GetComponent<NetworkIdentity>().connectionToClient, Plugin.TeleportMessage, Plugin.TeleportMessageDuration, false);
+                Plugin.addDebug("The teleportation clearly doesn't work");
+                return false;
+            }
+
+            Plugin.addDebug("Added SinkHole effect for componentInParent.");
+            componentInParent.EnableEffect("SinkHole");
+            return false;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,57 @@
+﻿using EXILED;
+using Harmony;
+
+namespace BetterSinkholes
+{
+    public class Plugin : EXILED.Plugin
+    {
+        public override string getName { get; } = "Better Sinkholes";
+
+        public static bool EnableBetterSinkholes;
+        public static float TeleportDistance;
+        public static float SlowDistance;
+        public static string TeleportMessage;
+        public static uint TeleportMessageDuration;
+        public static bool EnableDebug;
+
+        public static HarmonyInstance HarmonyInstance { set; get; }
+        private static uint HarmonyCounter = 0;
+        public override void OnEnable()
+        {
+            Plugin.Info("Whatever.");
+            EnableBetterSinkholes = Plugin.Config.GetBool("bs_enable", true);
+
+            if(EnableBetterSinkholes == false)
+            {
+                Plugin.Info("BetterSinkholes is disabled on this server.");
+                return;
+            }
+
+            SlowDistance = Plugin.Config.GetFloat("bs_slow_distance", 1.15f);
+            TeleportDistance = Plugin.Config.GetFloat("bs_teleport_distance", 0.7f);
+            TeleportMessage = Plugin.Config.GetString("bs_teleport_message", null);
+            TeleportMessageDuration = Plugin.Config.GetUInt("bs_teleport_message_duration", 5U);
+            EnableDebug = Plugin.Config.GetBool("bs_enable_debug", true);
+
+            HarmonyInstance = HarmonyInstance.Create($"blackruby.exiled.bettersinkholes");
+            HarmonyInstance.PatchAll();
+
+        }
+
+        public override void OnDisable()
+        {
+            HarmonyInstance.UnpatchAll();
+        }
+
+        public override void OnReload()
+        {
+            
+        }
+
+        public static void addDebug(string message)
+        {
+            if (Plugin.EnableDebug != true) return;
+            Plugin.Info(message);
+        }
+    }
+}
